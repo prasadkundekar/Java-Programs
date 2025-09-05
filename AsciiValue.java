61b39d654832eba419218a4da55ea2cf33286803
Prasad Kundekar <prasadkundekar292@gmail.com>
2025-09-05 18:24:34 +0530

    Added Ascii Value Finder Program

---
+++ AsciiValue.java
@@ -0,0 +1,8 @@
+public class AsciiValue {
+    public static void main(String[] args) {
+        char ch = 'f'; 
+        int ascii = ch; 
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}   
